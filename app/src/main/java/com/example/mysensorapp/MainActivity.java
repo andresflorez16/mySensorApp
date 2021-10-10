c01e4b59fd8d69b55acdf59891c7b4ad22b27a4e
andresflorez16 <andresflorez.dev@gmail.com>
2021-10-09 23:26:01 -0500

    firestore

--- app/src/main/java/com/example/mysensorapp/MainActivity.java
+++ app/src/main/java/com/example/mysensorapp/MainActivity.java
@@ -1,15 +1,72 @@
 package com.example.mysensorapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseFirestore db;
+    DatabaseReference myRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initialize();
+    }
+
+    public void initialize() {
+        FirebaseApp.initializeApp(this);
+        db = FirebaseFirestore.getInstance();
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Ada");
+        user.put("last", "Lovelace");
+        user.put("born", 1815);
+
+// Add a new document with a generated ID
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        System.out.println("DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        System.out.println("error");
+                    }
+                });
+
+    }
+
+    public void insertUser(String email, String name, String lastname, String phone) {
+        User user = new User();
+        user.setId(UUID.randomUUID().toString());
+        user.setEmail(email);
+        user.setName(name);
+        user.setLastname(lastname);
+        user.setPhone(phone);
+
+        myRef.child("users").child(user.getId()).setValue(user);
+
     }
 
 }
