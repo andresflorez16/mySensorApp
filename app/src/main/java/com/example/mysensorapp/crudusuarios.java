41518db16b6ea7712a158638caa5d8f559e147bb
FrogerXD <daniariot@gmail.com>
2021-10-12 21:42:38 -0500

    interfaz crud

---
+++ app/src/main/java/com/example/mysensorapp/crudusuarios.java
@@ -0,0 +1,14 @@
+package com.example.mysensorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class crudusuarios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crudusuarios);
+    }
+}
