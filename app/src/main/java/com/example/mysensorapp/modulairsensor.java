a894e95bda590a90f3314246f06937ac7b493951
FrogerXD <daniariot@gmail.com>
2021-10-13 21:21:29 -0500

    interfaz panel cliente, modulair, clarity sensor

---
+++ app/src/main/java/com/example/mysensorapp/modulairsensor.java
@@ -0,0 +1,14 @@
+package com.example.mysensorapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class modulairsensor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modulairsensor);
+    }
+}
