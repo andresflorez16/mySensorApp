99e5da90134c4514c5e0ce96abc0e640c7adc4ec
FrogerXD <daniariot@gmail.com>
2021-11-15 12:21:07 -0500

    datos

---
+++ app/src/main/java/com/example/mysensorapp/mhzsensor.java
@@ -0,0 +1,14 @@
+package com.example.mysensorapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class mhzsensor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mhzsensor);
+    }
+}
